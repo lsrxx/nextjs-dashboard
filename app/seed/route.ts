6f0803774e5640aa4d09fdde4466eeeb91a2cf19
lishirong <jack@51zpay.cn>
2025-04-11 17:24:40 +0800

    feat: 添加数据库执行文件

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,3 +1,4 @@
+// require('dotenv').config();
 // import bcrypt from 'bcrypt';
 // import { db } from '@vercel/postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
@@ -104,7 +105,7 @@
 export async function GET() {
   return Response.json({
     message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
   });
   // try {
   //   await client.sql`BEGIN`;
